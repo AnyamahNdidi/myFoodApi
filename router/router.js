54fae509de72d7a8618238b9627b6d8c5306a4e3
AnyamahNdidi <theo4felix@gmail.com>
2021-06-22 17:39:34 +0100

    the new update

--- router/router.js
+++ router/router.js
@@ -1,12 +1,13 @@
 const express = require("express")
 
-const { postFood, getAllFood, getFoodbyId } = require("../Controller/controller")
+const { postFood, getAllFood, getFoodbyId, deleteFoodId } = require("../Controller/controller")
 
 const router = express.Router()
 
 router.post("/foodpost", postFood)
 router.get("/all", getAllFood)
 router.get("/myfood/:id", getFoodbyId)
+router.delete("/mydel/:id", deleteFoodId)
 
 module.exports = router
 
